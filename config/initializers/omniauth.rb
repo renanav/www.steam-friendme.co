ed2de710885e4201339f7b10aeab92740fce9d80
renanav <renan.avrahami@gmail.com>
2016-03-12 15:23:12 -0500

    Editing omniauth.rb to try and solve opening on heroku

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,3 +1,11 @@
+# Rails.application.config.middleware.use OmniAuth::Builder do
+#   provider :steam, Rails.application.secrets.STEAM_WEB_API_KEY
+# end
+
+# Added to try and solve heroku issues
+require "omniauth/strategies/steam"
+require "openid/store/filesystem"
+
 Rails.application.config.middleware.use OmniAuth::Builder do
-  provider :steam, Rails.application.secrets.STEAM_WEB_API_KEY
+  provider :steam, Rails.application.secrets.steam_web_api_key, :storage => OpenID::Store::Filesystem.new("/tmp")
 end
